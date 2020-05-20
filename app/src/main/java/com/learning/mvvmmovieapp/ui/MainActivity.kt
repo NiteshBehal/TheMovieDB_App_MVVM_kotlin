2a341cc6f10f2cee141ca8709a60ff11798cf292
Nitesh Behal <nitesh.behal@publicissapient.com>
2020-05-20 21:30:03 +0530

    NavController initialization changes

--- app/src/main/java/com/learning/mvvmmovieapp/ui/MainActivity.kt
+++ app/src/main/java/com/learning/mvvmmovieapp/ui/MainActivity.kt
@@ -3,23 +3,24 @@ package com.learning.mvvmmovieapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.learning.mvvmmovieapp.R
 import com.learning.mvvmmovieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController: NavController by lazy { findNavController(R.id.fragment_container) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navController = Navigation.findNavController(this, R.id.fragment_container)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(
-            Navigation.findNavController(this, R.id.fragment_container)
+            navController
             , null
         )
     }
